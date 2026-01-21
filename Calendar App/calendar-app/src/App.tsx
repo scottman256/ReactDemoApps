68acfee7bb5d14906feb1559e40044a61a796d4e
scottman256 <scottmartinhall@gmail.com>
2026-01-20 19:33:22 -0500

    Lesson 4 Complete

---
+++ Calendar App/calendar-app/src/App.tsx
@@ -0,0 +1,10 @@
+import CalendarApp from "./Components/CalendarApp"
+import './Components/CalendarApp.css'
+
+const App = () => {
+  return (
+    <div className="container"><CalendarApp /></div>
+  )
+}
+
+export default App
