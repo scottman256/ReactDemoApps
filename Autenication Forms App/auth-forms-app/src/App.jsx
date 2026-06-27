35e6c0606fa61aa253f1307df4f4514218285a4d
scottman256 <scottmartinhall@gmail.com>
2026-06-27 19:03:32 -0400

    Section 15: Authenication Forms

---
+++ Autenication Forms App/auth-forms-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignInSignUp from './components/SignInSignUp'
+
+const App = () => {
+  return (
+    <div>
+      <SignInSignUp />
+    </div>
+  )
+}
+
+export default App
